39fe82aa41ed11e15cfad473c58c0cc95c955114
Stefan Majewsky <majewsky@gmx.net>
2016-01-14 20:48:36 +0100

    build_package_with_holo.pl: sign generated packages

--- build_package_with_holo.pl
+++ build_package_with_holo.pl
@@ -28,13 +28,31 @@ while (<$fh>) {
 }
 close($fh);
 
+# read makepkg.conf to see if signing is wanted
+my ($sign, $gpg_key) = (0, undef);
+open $fh, '<', '/etc/makepkg.conf';
+while (<$fh>) {
+   if (/^\s*BUILDENV\s*=\s*\((.+?)\)\s*$/) {
+      $sign = $1 =~ /\b(?<!!)sign\b/;
+   }
+   elsif (/^\s*GPGKEY\s*=\s*"(.+?)"\s*$/) {
+      $gpg_key = $1;
+   }
+}
+close($fh);
+
 ################################################################################
 # build package if it's not yet there
 #
 # (we're running inside the repo directory, so auto-output is fine)
 
 my $package_file = "$pkgname-$pkgver-$pkgrel-any.pkg.tar.xz";
-system("holo-build <$filename") unless -f $package_file;
+if (not -f $package_file) {
+   system("holo-build <$filename");
+}
+if ($sign and not -f "$package_file.sig") {
+   system("gpg", "--detach-sign", "--use-agent", ($gpg_key ? ("-u", $gpg_key) : ()), "--no-armor", $package_file);
+}
 
 ################################################################################
 # clean earlier versions of this package
