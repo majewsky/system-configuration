edfacb00b51ccf5044ee5236848efd475a02d997
Stefan Majewsky <majewsky@gmx.net>
2016-09-30 19:51:38 +0200

    hologram-automation: skip health report on interactive systems unless error

--- src/automation-health.sh
+++ src/automation-health.sh
@@ -8,11 +8,18 @@ systemctl --failed | sed '/^$/,$d;/loaded units listed/,$d;1d' | cut -d' ' -f2 >
 if grep -q '[a-z]' /tmp/failed-units; then
     echo ":: systemctl lists $(wc -l < /tmp/failed-units) unit(s) in an error state:"
     cat /tmp/failed-units
-    rm  /tmp/failed-units
     SUCCESS=0
 fi
+rm -f /tmp/failed-units
 
 # TODO: check for .pacnew files (with whitelist for passwd, shadow, etc.)
+
+# skip summary for interactive systems if no errors are occurred
+if [ $SUCCESS = 1 ]; then
+    if systemctl show display-manager.service 2>/dev/null | grep -q 'SubState=running'; then
+        exit 0
+    fi
+fi
 
 # report summary
 if [ $SUCCESS = 0 ]; then
