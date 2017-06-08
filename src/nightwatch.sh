337f02a91115d0474dbd26e26a09ee74e5d630d6
Stefan Majewsky <majewsky@gmx.net>
2017-06-08 10:00:19 +0200

    nightwatch: fix number parsing error, calculate sleep interval with second precision

--- src/nightwatch.sh
+++ src/nightwatch.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-set -euo pipefail
+set -xeuo pipefail
 
 NIGHTWATCH_END=360    # 6:00 = 360 minutes after midnight
 NIGHTWATCH_START=1380 # 23:00 = 1380 minutes after midnight
 
-# get minutes since midnight
-CURRENT=$(($(date +'%H*60+%M')))
-echo "CURRENT = $CURRENT"
+# get minutes since midnight (the sed converts "08" and "09" into "8" and "9" because $((...)) would choke on them otherwise)
+CURRENT=$(($(date +'%H*3600+%M*60+%S' | sed 's/0\([89]\)/\1/g')))
 # if started during the day, sleep until 23:00
-if [ $CURRENT -ge $NIGHTWATCH_END -a $CURRENT -lt $NIGHTWATCH_START ]; then
-  MINS=$((NIGHTWATCH_START - CURRENT))
-  echo "Sleeping for $MINS minutes..."
-  sleep $((MINS * 60))
+if [ $CURRENT -ge $((NIGHTWATCH_END * 60)) -a $CURRENT -lt $((NIGHTWATCH_START * 60)) ]; then
+  sleep $((NIGHTWATCH_START * 60 - CURRENT))
 fi
 
 if pidof Xorg >/dev/null; then
