7afce919713569add5f5bd5797a76980d47158da
Stefan Majewsky <majewsky@gmx.net>
2017-09-25 21:34:59 +0200

    hologram-base: update unpack-secrets.sh to handle base64-encoded secrets

--- src/unpack-secrets.sh
+++ src/unpack-secrets.sh
@@ -8,8 +8,11 @@ if [ -z "${KEY}" ]; then
 fi
 
 find /etc/replicator.d -name \*.toml -delete
-for FILE in "/etc/secrets/$(echo -n "$(cat /etc/hostname)" | sha256sum | cut -d' ' -f1)"/*.gpg; do
-  OUTFILE="/etc/replicator.d/$(basename "${FILE}" .gpg).toml"
-  echo "${KEY}" | gpg --pinentry-mode loopback --quiet --decrypt --passphrase-fd 0 -o "${OUTFILE}" "${FILE}"
+for FILE in "/etc/secrets/$(echo -n "$(cat /etc/hostname)" | sha256sum | cut -d' ' -f1)"/*.gpg.b64; do
+  OUTFILE="/etc/replicator.d/$(basename "${FILE}" .gpg.b64).toml"
+  GPGFILE="$(basename "${FILE}" .b64)"
+  base64 -d < "${FILE}" > "${GPGFILE}"
+  echo "${KEY}" | gpg --pinentry-mode loopback --quiet --decrypt --passphrase-fd 0 -o "${OUTFILE}" "${GPGFILE}"
   chmod 0600 "${OUTFILE}"
+  rm "${GPGFILE}" # cleanup temporary file
 done
