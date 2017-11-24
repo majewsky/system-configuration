01ac7780a217f20182941ccab9c5aa81aea87431
Stefan Majewsky <majewsky@gmx.net>
2017-11-24 17:17:39 +0100

    hologram-minecraft-server: use systemd for process control

--- src/minecraftServerLauncher.sh
+++ src/minecraftServerLauncher.sh
@@ -1,76 +1,14 @@
 #!/bin/bash
+set -euo pipefail
 
-# CONFDIR="$HOME/.minecraftServerLauncher/"
-# if [ ! -d "$CONFDIR" ]; then
-# 	mkdir "$CONFDIR"
-# fi
+SERVER_ROOT="$HOME/servers"
+CURRENT_SERVER="$(systemctl --user | awk '$4 == "running" && $1 ~ /minecraft@/ { print $1 }' | sed 's/^minecraft@//;s/\.service$//' || exit 1)"
 
-CONFFILE="$HOME/.minecraftServerLauncherrc"
-if [ -f "$CONFFILE" ]; then
-	. "$CONFFILE"
-else
-	SERVERROOT="$HOME/minecraft"
-fi
-
-STATEFILE="$XDG_RUNTIME_DIR/minecraftServerState"
-LOCKFILE="$XDG_RUNTIME_DIR/minecraftServerLock"
-
-exitHandler()
-{
-	rm "$LOCKFILE"
-	flock -u 9
-	# echo EXIT
-	if [ -z "$1" ]; then
-		exit 0
-	else
-		exit $1 || exit 1
-	fi
-}
-
-showAvail()
-{
-	echo "Available Servers:"
-	ls "$SERVERROOT"
-}
-
-lock()
-{
-	exec 9> "$LOCKFILE"
-	if ! flock -n 9 ; then
-		echo "Locked by other process."
-		exit 1
-	fi
-	trap exitHandler EXIT
-}
-
-getState()
-{
-	if [ ! -f "$STATEFILE" ]; then
-		CURRENTSERVER=""
-	else
-		CURRENTSERVER=$( cat "$STATEFILE" )
-		set $CURRENTSERVER
-		CS_NAME=$1
-		CS_PID=$2
-	fi
-}
-
-if [ ! -z $SSH_ORIGINAL_COMMAND ]; then
-	set $SSH_ORIGINAL_COMMAND
+if [ -n "${SSH_ORIGINAL_COMMAND:-}" ]; then
+	set -- $SSH_ORIGINAL_COMMAND
 fi
 CMD=$1
 shift
-ARGS=$@
-
-getState
-
-if [ -z "$CURRENTSERVER" ]; then
-	echo "No Server running."
-else
-	echo "Currently running server: $CURRENTSERVER"
-fi
-
-showAvail
 
 CMDS_HELP="Available commands: ( stop ) ( start [servername] ) ( logs )"
 if [ -z "$CMD" ]; then
@@ -79,55 +17,43 @@ if [ -z "$CMD" ]; then
 fi
 
 case "$CMD" in
+	list)
+		cd "$SERVER_ROOT"
+		ls --quoting-style=literal -1 */Server\ Files/ServerStart.sh | cut -d/ -f1
+		;;
 	stop)
-		if [ "$ARGS" != "-f" ]; then
-			lock
+		if [ -z "$CURRENT_SERVER" ]; then
+			echo "No server running."
+			exit 0
 		fi
-		getState
-		if [ -z "$CURRENTSERVER" ]; then
-			echo "stop: No server running."
-			exit 1
-		fi
-		echo "stop Shutting down server $CURRENTSERVER"
-		kill -s SIGTERM $CS_PID
-		rm $STATEFILE
-		CURRENTSERVER=""
+		echo "Stopping $CURRENT_SERVER..."
+		systemctl --user stop "minecraft@$CURRENT_SERVER"
 		;;
 	start)
-		lock
-		getState
-		S_NAME="$ARGS"
-		if [ ! -d "$SERVERROOT/$S_NAME" ]; then
-			echo Invalid server name $S_NAME
-			showAvail
+		if [ ! -f "$SERVER_ROOT/$1/Server Files/ServerStart.sh" ]; then
+			echo "Invalid server name: $1"
 			exit 1
 		fi
-		if [ ! -z "$CURRENTSERVER" ]; then
-			if [ "$CS_NAME" = "$S_NAME" ]; then
-				echo Server $S_NAME slready running.
+		if [ -n "$CURRENT_SERVER" ]; then
+			if [ "$CURRENT_SERVER" = "$1" ]; then
+				echo "Server $1 already running."
 				exit 0
 			else
-				echo Shutting down running server.
-				kill -s SIGTERM $CS_PID
+				echo "Server $CURRENT_SERVER currently running. Use \`stop\` command first."
+				exit 1
 			fi
 		fi
-		cd "$SERVERROOT/$S_NAME/Server Files/" || exit 1
-		/bin/sh "ServerStart.sh" >& log  &
-		CS_PID=$!
-		CS_NAME=$S_NAME
-		CURRENTSERVER="$CS_NAME $CS_PID"
-		echo "start: $CURRENTSERVER"
-		echo "$CURRENTSERVER" > "$STATEFILE"
+		systemctl --user start "minecraft@$1"
+		echo "Server $1 now starting up."
 		;;
 	logs)
-		if [ -z "$CURRENTSERVER" ]; then
-			echo "No Server running"
-		else
-			tail -f "$SERVERROOT/$CS_NAME/Server Files/log"
+		if [ -z "$CURRENT_SERVER" ]; then
+			echo "No server running."
+			exit 1
 		fi
+		exec journalctl -q --user -u "minecraft@$CURRENT_SERVER" "$@"
 		;;
 	*)
-		echo "Invalid command: $CMD"
 		echo "$CMDS_HELP"
 		exit 1
 		;;
