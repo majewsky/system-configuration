cdf233704f2cdab7256d63037b2dd56c22b5b321
Stefan Majewsky <majewsky@gmx.net>
2017-11-24 17:31:18 +0100

    minecraft-shell: add status command, better usage message

--- src/minecraft-shell.sh
+++ src/minecraft-shell.sh
@@ -4,24 +4,52 @@ set -euo pipefail
 SERVER_ROOT="$HOME/servers"
 CURRENT_SERVER="$(systemctl --user | awk '$4 == "running" && $1 ~ /minecraft@/ { print $1 }' | sed 's/^minecraft@//;s/\.service$//' || exit 1)"
 
-if [ -n "${SSH_ORIGINAL_COMMAND:-}" ]; then
-	set -- $SSH_ORIGINAL_COMMAND
-fi
-CMD=$1
-shift
+usage() {
+cat <<-'EOF'
+syntax:
+	minecraft-shell list            - List names of available servers.
+	                                  The mark indicates the currently running server, if any.
 
-CMDS_HELP="Available commands: ( stop ) ( start [servername] ) ( logs )"
-if [ -z "$CMD" ]; then
-	echo "$CMDS_HELP"
+	minecraft-shell start <NAME>    - Start given server.
+	minecraft-shell stop            - Stop currently running server, if any.
+
+	minecraft-shell status          - Show status of currently running server, if any.
+	minecraft-shell logs [ARG...]   - Show logs for currently running server.#
+	                                  Args are passed on to journalctl(1).
+EOF
+}
+
+if [ -n "${SSH_ORIGINAL_COMMAND:-}" ]; then
+	set -- "${SSH_ORIGINAL_COMMAND}"
+fi
+if [ $# -eq 0 ]; then
+	usage
 	exit 0
 fi
 
+CMD="$1"
+shift
+
 case "$CMD" in
 	list)
+		if [ $# -ne 0 ]; then
+			usage
+			exit 1
+		fi
 		cd "$SERVER_ROOT"
-		ls --quoting-style=literal -1 */Server\ Files/ServerStart.sh | cut -d/ -f1
+		ls --quoting-style=literal -1 */Server\ Files/ServerStart.sh | cut -d/ -f1 | while read SERVER_NAME; do
+			if [ "$CURRENT_SERVER" = "$SERVER_NAME" ]; then
+				echo "* $SERVER_NAME"
+			else
+				echo "  $SERVER_NAME"
+			fi
+		done
 		;;
 	stop)
+		if [ $# -ne 0 ]; then
+			usage
+			exit 1
+		fi
 		if [ -z "$CURRENT_SERVER" ]; then
 			echo "No server running."
 			exit 0
@@ -30,13 +58,17 @@ case "$CMD" in
 		systemctl --user stop "minecraft@$CURRENT_SERVER"
 		;;
 	start)
+		if [ $# -ne 1 ]; then
+			usage
+			exit 1
+		fi
 		if [ ! -f "$SERVER_ROOT/$1/Server Files/ServerStart.sh" ]; then
-			echo "Invalid server name: $1"
+			echo "No such server: $1"
 			exit 1
 		fi
 		if [ -n "$CURRENT_SERVER" ]; then
 			if [ "$CURRENT_SERVER" = "$1" ]; then
-				echo "Server $1 already running."
+				echo "Already running."
 				exit 0
 			else
 				echo "Server $CURRENT_SERVER currently running. Use \`stop\` command first."
@@ -46,6 +78,17 @@ case "$CMD" in
 		systemctl --user start "minecraft@$1"
 		echo "Server $1 now starting up."
 		;;
+	status)
+		if [ $# -ne 0 ]; then
+			usage
+			exit 1
+		fi
+		if [ -z "$CURRENT_SERVER" ]; then
+			echo "No server running."
+			exit 1
+		fi
+		systemctl --user status "minecraft@$CURRENT_SERVER"
+		;;
 	logs)
 		if [ -z "$CURRENT_SERVER" ]; then
 			echo "No server running."
@@ -54,7 +97,7 @@ case "$CMD" in
 		exec journalctl -q --user -u "minecraft@$CURRENT_SERVER" "$@"
 		;;
 	*)
-		echo "$CMDS_HELP"
+		usage
 		exit 1
 		;;
 esac
